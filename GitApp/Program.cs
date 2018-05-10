fdf9b2e4aff71c199dbbea165dcbd0e4f03b6261
tserk <tserk@DESKTOP-F8E0RAA>
2018-05-10 22:56:23 +0300

    C#

--- GitApp/Program.cs
+++ GitApp/Program.cs
@@ -4,12 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GitApp
+namespace KateVakurova
 {
-    class Program
+    public class A
     {
-        static void Main(string[] args)
+        public int a = 27;
+        public int b = 9;
+        public int c
         {
+            get
+            {
+                if (a > b) return a;
+                else return b;
+            }
+            set
+            {
+
+            }
+        }
+        public class B : A
+        {
+            public int d = 7;
+
+            public int c2
+            {
+                get
+                {
+                    return d %= +d;
+                }
+            }
+        }
+        class Programm
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("лаба 4");
+
+                A obj1 = new A();
+                Console.WriteLine(" Ответ 1: " + obj1.c);
+                B obj2 = new B();
+                Console.WriteLine(" Ответ 2: " + obj2.c2);
+                Console.ReadKey();
+            }
         }
     }
 }
